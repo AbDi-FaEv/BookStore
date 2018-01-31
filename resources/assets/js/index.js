c61e336d9f04f967f37d3fa00aad625ec740cd71
Dmitry Abramenko <abramenko@a2design.biz>
2018-01-31 16:58:50 +0600

    Setup react framework in project

---
+++ resources/assets/js/index.js
@@ -0,0 +1,13 @@
+import './bootstrap';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
+
+
+require('./components/Example');
