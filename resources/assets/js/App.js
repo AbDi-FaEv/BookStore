5e7a1b0849e230397f78273d0ec0b577f1872fbb
Dmitry Abramenko <abramenko@a2design.biz>
2018-01-31 16:59:00 +0600

    Setup react framework in project

---
+++ resources/assets/js/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+    render() {
+        return (
+            <h1 className='header'>Hello, world!</h1>
+        );
+    }
+}
+
+export default App;
--- resources/assets/js/index.js
+++ resources/assets/js/index.js
@@ -4,10 +4,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
-
-
-require('./components/Example');
